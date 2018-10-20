d3603e32d43257910cc0c54db122508b0604cee4
Tae Yoon Kim <kim2942@purdue.edu>
2018-10-20 12:44:30 -0400

    fixed the startup glitch

--- app/src/main/java/com/boilermake/foodshare/foodshare_boilermake2018/mainPage.java
+++ app/src/main/java/com/boilermake/foodshare/foodshare_boilermake2018/mainPage.java
@@ -1,27 +1,17 @@
 package com.boilermake.foodshare.foodshare_boilermake2018;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class mainPage extends AppCompatActivity {
     Toolbar toolbar;
     private FirebaseAuth auth;
-    String userID = auth.getCurrentUser().getUid();
-    DatabaseReference currentUserDB = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
 
     //TODO: Retrieve user data (User name, City)
     public String profileName;
