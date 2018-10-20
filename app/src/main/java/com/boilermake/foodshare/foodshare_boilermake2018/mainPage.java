12368526c1b7f60f2bc436d0304778288256cb55
Tae Yoon Kim <kim2942@purdue.edu>
2018-10-20 00:43:31 -0400

    editted styles.xml

--- app/src/main/java/com/boilermake/foodshare/foodshare_boilermake2018/mainPage.java
+++ app/src/main/java/com/boilermake/foodshare/foodshare_boilermake2018/mainPage.java
@@ -1,4 +1,13 @@
 package com.boilermake.foodshare.foodshare_boilermake2018;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class mainPage {
 }
